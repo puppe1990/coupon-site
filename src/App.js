189c82f1913795f61645fa8a8acdadd81e0c8de1
puppe1990 <matheus.puppe90@hotmail.com>
2023-02-24 16:52:11 -0300

    add copy and paster coupon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { FaCopy } from "react-icons/fa";
 
 function App() {
   const [selectedCompany, setSelectedCompany] = useState(null);
+  const [copied, setCopied] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const companies = [
     {
       name: "Company 1",
       logo: "https://via.placeholder.com/100x100",
       discount: "20% off",
-      couponCode: "CODE123"
+      couponCode: "CODE123",
+      link: "",
     },
     {
       name: "Company 2",
       logo: "https://via.placeholder.com/100x100",
       discount: "10% off",
-      couponCode: "CODE456"
+      couponCode: "CODE456",
+      link: "",
     },
     // add more companies here
   ];
+
+  const filteredCompanies = companies.filter((company) =>
+    company.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   function showModal(company) {
     setSelectedCompany(company);
@@ -27,6 +37,11 @@ function App() {
 
   function closeModal() {
     setSelectedCompany(null);
+  }
+
+  function handleCopy() {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
   }
 
   return (
@@ -37,12 +52,26 @@ function App() {
           <p className="lead text-white">
             Find the best deals and discounts from your favorite companies.
           </p>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search for company"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <div className="input-group-append">
+              <button className="btn btn-primary" type="button">
+                Search
+              </button>
+            </div>
+          </div>
         </div>
       </header>
       <div className="bg-light py-5">
         <div className="container">
           <div className="row">
-            {companies.map((company) => (
+            {filteredCompanies.map((company) => (
               <div key={company.name} className="col-md-4 mb-4">
                 <div className="card border-0">
                   <img
@@ -53,12 +82,17 @@ function App() {
                   <div className="card-body">
                     <h3 className="card-title text-primary">{company.name}</h3>
                     <p className="card-text">{company.discount}</p>
-                    <button
-                      className="btn btn-orange"
-                      onClick={() => showModal(company)}
+                    <CopyToClipboard
+                      text={company.couponCode}
+                      onCopy={handleCopy}
                     >
-                      Show Coupon
-                    </button>
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => showModal(company)}
+                      >
+                        Show Coupon <FaCopy className="ml-2" />
+                      </button>
+                    </CopyToClipboard>
                   </div>
                 </div>
               </div>
@@ -70,9 +104,25 @@ function App() {
                 <h3>{selectedCompany.name}</h3>
                 <p>{selectedCompany.discount}</p>
                 <p>Coupon code: {selectedCompany.couponCode}</p>
-                <button className="btn btn-orange" onClick={closeModal}>
-                  Close
-                </button>
+                <p>Link: {selectedCompany.link}</p>
+                <div className="text-center">
+                  <CopyToClipboard
+                    text={selectedCompany.couponCode}
+                    onCopy={handleCopy}
+                  >
+                    <button className="btn btn-primary">
+                      Copy Coupon Code <FaCopy className="ml-2" />
+                    </button>
+                  </CopyToClipboard>
+                  {copied && (
+                    <div className="text-success mt-2">
+                      Copied to clipboard!
+                    </div>
+                  )}
+                  <button className="btn btn-secondary" onClick={closeModal}>
+                    Close
+                  </button>
+                </div>
               </div>
             </div>
           )}
