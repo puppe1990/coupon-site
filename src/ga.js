e25a1b8d89f8ef79aa6799549506da7b008240fa
puppe1990 <matheus.puppe90@hotmail.com>
2023-02-27 14:43:28 -0300

    change env

--- src/ga.js
+++ src/ga.js
@@ -1,7 +1,7 @@
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+  ReactGA.initialize(process.env.GOOGLE_ANALYTICS_ID);
 };
 
 export const logPageView = () => {
