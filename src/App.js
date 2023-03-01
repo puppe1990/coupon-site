96b3992c9bf9f82b5ccf1ac5cb9ff7e3adf983e7
puppe1990 <matheus.puppe90@hotmail.com>
2023-03-01 15:05:28 -0300

    only show clipboard with code is active

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import { initGA, logPageView } from './ga';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import { FaCopy } from "react-icons/fa";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { FaCopy } from 'react-icons/fa';
 import image1 from './images/yuool.png';
 import image2 from './images/mercado_diferente.png';
 import image3 from './images/insider.png';
@@ -19,57 +19,58 @@ function App() {
   }, []);
   const [selectedCompany, setSelectedCompany] = useState(null);
   const [copied, setCopied] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
 
   const companies = [
     {
-      name: "Cupom Yuool",
+      name: 'Cupom Yuool',
       logo: image1,
-      discount: "15% de desconto Primeira Compra",
-      couponCode: "AFA355FFAE",
-      link: "https://www.yuool.com.br/?referrer_token=eRapyM",
+      discount: '15% de desconto Primeira Compra',
+      couponCode: 'AFA355FFAE',
+      link: 'https://www.yuool.com.br/?referrer_token=eRapyM',
     },
     {
-      name: "Cupom Mercado Diferente",
+      name: 'Cupom Mercado Diferente',
       logo: image2,
-      discount: "15% de desconto Primeira Compra na primeira cesta",
-      couponCode: "",
-      link: "https://mercdif.me/matheus-nunes-puppe",
+      discount: '15% de desconto Primeira Compra na primeira cesta',
+      couponCode: '',
+      link: 'https://mercdif.me/matheus-nunes-puppe',
     },
     {
-      name: "Cupom Insider",
+      name: 'Cupom Insider',
       logo: image3,
-      discount: "R$30 OFF em compras acima de R$150",
-      couponCode: "",
-      link: "https://www.insiderstore.com.br/a/rewards/r/rx1mc3Fv",
+      discount: 'R$30 OFF em compras acima de R$150',
+      couponCode: '',
+      link: 'https://www.insiderstore.com.br/a/rewards/r/rx1mc3Fv',
     },
     {
-      name: "Cupom Zé Delivery",
+      name: 'Cupom Zé Delivery',
       logo: image4,
-      discount: "Cupom de R$10 na primeira compra",
-      couponCode: "MAPUPPJYG",
-      link: "https://ze.sng.link/Dm9m7/5q7i/r_07d9c4b7a3",
+      discount: 'Cupom de R$10 na primeira compra',
+      couponCode: 'MAPUPPJYG',
+      link: 'https://ze.sng.link/Dm9m7/5q7i/r_07d9c4b7a3',
     },
     {
-      name: "Cupom Zee.now",
+      name: 'Cupom Zee.now',
       logo: image5,
-      discount: "ganhe R$25 de desconto",
-      couponCode: "JJOASW",
-      link: "https://zeenow.app.link/FFtaECeYh4",
+      discount: 'ganhe R$25 de desconto',
+      couponCode: 'JJOASW',
+      link: 'https://zeenow.app.link/FFtaECeYh4',
     },
     {
-      name: "Cupom Cornershop",
+      name: 'Cupom Cornershop',
       logo: image6,
-      discount: "Frete Grátis e bônus de R$10 na primeira compra",
-      couponCode: "",
-      link: "https://corner.shop/r/6atd4nffm",
+      discount: 'Frete Grátis e bônus de R$10 na primeira compra',
+      couponCode: '',
+      link: 'https://corner.shop/r/6atd4nffm',
     },
     {
-      name: "Cupom Zee Dog",
+      name: 'Cupom Zee Dog',
       logo: image7,
-      discount: "15% de desconto para os amigos desavisados que nunca compraram na Zee.Dog",
-      couponCode: "",
-      link: "http://zeedog.refr.cc/matheusnunespuppe ",
+      discount:
+        '15% de desconto para os amigos desavisados que nunca compraram na Zee.Dog',
+      couponCode: '',
+      link: 'http://zeedog.refr.cc/matheusnunespuppe ',
     },
   ];
 
@@ -150,28 +151,35 @@ function App() {
                 <h3>{selectedCompany.name}</h3>
                 <p>{selectedCompany.discount}</p>
                 {selectedCompany.couponCode && (
-                   <p>Código Cupom: {selectedCompany.couponCode}</p>
+                  <>
+                    <p>Código Cupom: {selectedCompany.couponCode}</p>
+                    <div className="text-center">
+                      {copied && (
+                        <div className="text-success mt-2">
+                          Copiado para a área de transferência!
+                        </div>
+                      )}
+                      <CopyToClipboard
+                        text={selectedCompany.couponCode}
+                        onCopy={handleCopy}
+                      >
+                        <button className="btn btn-primary">
+                          Copiar código do cupom
+                          <FaCopy className="ml-2" />
+                        </button>
+                      </CopyToClipboard>
+                    </div>
+                  </>
                 )}
                 {selectedCompany.link && (
                   <p>
-                    Link: <a href={selectedCompany.link}>Ir direto para o site!</a>
+                    Link:{' '}
+                    <a href={selectedCompany.link} target="_blank" rel="noopener noreferrer">
+                      Ir direto para o site!
+                    </a>
                   </p>
                 )}
                 <div className="text-center">
-                {copied && (
-                    <div className="text-success mt-2">
-                      Copiado para a área de transferência!
-                    </div>
-                  )}
-                  <CopyToClipboard
-                    text={selectedCompany.couponCode}
-                    onCopy={handleCopy}
-                  >
-                    <button className="btn btn-primary">
-                      Copiar código do cupom<FaCopy className="ml-2" />
-                    </button>
-                  </CopyToClipboard>
-
                   <button className="btn btn-secondary" onClick={closeModal}>
                     Fechar
                   </button>
