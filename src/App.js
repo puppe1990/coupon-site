f094289f22db5a6a20887cdea5911b2e429e02ce
puppe1990 <matheus.puppe90@hotmail.com>
2023-03-27 15:23:50 -0300

    loans list

--- src/App.js
+++ src/App.js
@@ -4,18 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { FaCopy } from "react-icons/fa";
-import image1 from "./images/yuool.png";
-import image2 from "./images/mercado_diferente.png";
-import image3 from "./images/insider.png";
-import image4 from "./images/ze.png";
-import image5 from "./images/zee-now.png";
-import image6 from "./images/cornershop.png";
-import image7 from "./images/zee-dog.png";
-import image8 from "./images/petlove-plano.png";
-import image9 from "./images/recarga-pay.png";
-import image10 from "./images/99-viagem.png";
-import image11 from "./images/99-motorista.png";
-import image12 from "./images/woba.png";
+import image1 from "./images/biz.png";
+import image2 from "./images/inter.png";
 
 function App() {
   useEffect(() => {
@@ -28,90 +18,18 @@ function App() {
 
   const companies = [
     {
-      name: "Cupom Yuool",
+      name: "Empréstimo Biz Capital",
       logo: image1,
-      discount: "15% de desconto Primeira Compra",
-      couponCode: "AFA355FFAE",
-      link: "https://www.yuool.com.br/?referrer_token=eRapyM",
+      discount: "Empréstimo empresarial",
+      couponCode: "",
+      link: "https://parceiros.bizcapital.com.br/parceiro_6406",
     },
     {
-      name: "Cupom Mercado Diferente",
+      name: "Empréstimo Consignado Inter",
       logo: image2,
-      discount: "15% de desconto Primeira Compra na primeira cesta",
-      couponCode: "",
-      link: "https://mercdif.me/matheus-nunes-puppe",
-    },
-    {
-      name: "Cupom Insider",
-      logo: image3,
-      discount: "R$30 OFF em compras acima de R$150",
-      couponCode: "",
-      link: "https://www.insiderstore.com.br/a/rewards/r/rx1mc3Fv",
-    },
-    {
-      name: "Cupom Zé Delivery",
-      logo: image4,
-      discount: "Cupom de R$10 na primeira compra",
-      couponCode: "MAPUPPJYG",
-      link: "https://ze.sng.link/Dm9m7/5q7i/r_07d9c4b7a3",
-    },
-    {
-      name: "Cupom Zee.now",
-      logo: image5,
-      discount: "ganhe R$25 de desconto",
-      couponCode: "JJOASW",
-      link: "https://zeenow.app.link/FFtaECeYh4",
-    },
-    {
-      name: "Cupom Cornershop",
-      logo: image6,
-      discount: "Frete Grátis e bônus de R$10 na primeira compra",
-      couponCode: "",
-      link: "https://corner.shop/r/6atd4nffm",
-    },
-    {
-      name: "Cupom Zee Dog",
-      logo: image7,
-      discount:
-        "15% de desconto para os amigos desavisados que nunca compraram na Zee.Dog",
-      couponCode: "",
-      link: "http://zeedog.refr.cc/matheusnunespuppe ",
-    },
-    {
-      name: "Cupom Petlove Plano de saúde",
-      logo: image8,
-      discount: "20% OFF na 1ª mensalidade dos Planos de Saúde",
-      couponCode: "",
-      link: "https://saude.petlove.com.br/indicacao/laxi9ghpmmofhqew ",
-    },
-    {
-      name: "Cupom Recarga Pay",
-      logo: image9,
-      discount:
-        "Para ganhar R$20. É só baixar o App e fazer sua 1ª compra em Pix com qualquer cartão em até 12x",
-      couponCode: "matpup024",
-      link: "http://recargapay.com.br/r/matpup024-lO ",
-    },
-    {
-      name: "Cupom 99",
-      logo: image10,
-      discount: "Desconto primeira viagem",
-      couponCode: "",
-      link: "https://d.99app.com/RUdWne2",
-    },
-    {
-      name: "Convite 99 motorista",
-      logo: image11,
-      discount: "Você foi convidado para ser motorista parceiro 99! ",
-      couponCode: "",
-      link: "https://d.99app.com/e1ByJVE",
-    },
-    {
-      name: "Diária Woba",
-      logo: image12,
-      discount: "Você ganhou reserva grátis em qualquer espaço de coworking ",
-      couponCode: "",
-      link: "https://bnc.lt/woba-referral?rid=r43533",
+      discount: "Empréstimo Consignado Inter",
+      couponCode: "7066430B",
+      link: "https://www.youtube.com/watch?v=yk6-iuIUe6I",
     },
   ];
 
@@ -136,15 +54,15 @@ function App() {
     <div className="App">
       <header className="bg-primary py-4">
         <div className="container text-center">
-          <h1 className="text-white">Bem vindo ao Cupom Vantagens</h1>
+          <h1 className="text-white">Bem vindo a Unity Crédito</h1>
           <p className="lead text-white">
-            O seu destino para economizar com cupons de desconto
+            O auxílio financeiro que você precisa
           </p>
           <div className="input-group mb-3">
             <input
               type="text"
               className="form-control"
-              placeholder="Pesquisar empresa com cupom"
+              placeholder="Pesquisar empréstimo"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
